71c38a214a4ed34bd0d7635cddefa4126b783bf4
DaffoetxD <davidvravenswaaij@gmail.com>
2026-02-13 18:32:03 +0100

    balon die when 0 hp bum bum
    oh no bum bum no good

--- Assets/Scripts/Balloon/MoveBalloon.cs
+++ Assets/Scripts/Balloon/MoveBalloon.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class MoveBalloon : MonoBehaviour
 {
+    [SerializeField] private int health = 5;
     [SerializeField] private float moveSpeed = 2f;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         gameObject.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        CheckDeath();
+    }
+
+
+    private void CheckDeath()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        
+            //hier later nog SFX
+        }
+    
+    
     }
 }
